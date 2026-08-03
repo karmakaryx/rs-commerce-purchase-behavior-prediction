5606972f6cefea3f43eef38b37e104ccae4d0ae8
karmakaryx <karmakaryx@gmail.com>
2026-08-04 04:04:01 +0900

    feat: add utility function to set random seed

---
+++ code/utils.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import random
+import torch
+
+def set_seed(seed):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
